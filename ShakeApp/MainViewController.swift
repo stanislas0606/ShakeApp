daf5e26884ea51b1aa2e8f8aa5e42865cf912926
Stanislav Vorozhtsov <stanislav.vorozhtsov@softermii.com>
2021-10-20 23:05:04 +0300

    Add MainVC, SettingsVC

---
+++ ShakeApp/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  ShakeApp
+//
+//  Created by Softermii-User on 20.10.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
