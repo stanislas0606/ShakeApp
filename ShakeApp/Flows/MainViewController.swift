a108b9fe1ad5afd8b54090198e627c6be7fa70ea
Stanislav Vorozhtsov <stanislav.vorozhtsov@softermii.com>
2021-11-21 01:55:50 +0200

    Refactor MainVC via SnapKit

--- ShakeApp/Flows/MainViewController.swift
+++ ShakeApp/Flows/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
 
@@ -15,6 +16,9 @@ class MainViewController: UIViewController {
 
     private var networkDataProvider: NetworkDataProvider
     private let storageDataProvider: StorageDataProvider
+    
+    private let horizontalPadding = 20
+    private let verticalPadding = 24
 
     // MARK: - Init
     init(networkDataProvider: NetworkDataProvider, storageDataProvider: StorageDataProvider) {
@@ -30,13 +34,16 @@ class MainViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+        setupAnswerLabelCostraints()
+        setupQuestionTextFieldCostraints()
         setupViews()
     }
 
     override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         questionTextField.resignFirstResponder()
         networkDataProvider.delegate = self
-
     }
 
     override func motionCancelled(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
@@ -49,41 +56,6 @@ class MainViewController: UIViewController {
     }
 
     // MARK: - Private
-    private func setupViews() {
-        view.backgroundColor = Asset.Colors.whiteColor.color
-
-        self.navigationController?.navigationBar.topItem?.title = L10n.AppName.text
-        self.navigationController?.navigationBar.tintColor = Asset.Colors.grayColor.color
-        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(rigthButtonClicked))
-
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
-        view.addGestureRecognizer(tap)
-
-        view.addSubview(answerLabel)
-        view.addSubview(questionTextField)
-
-        answerLabel.translatesAutoresizingMaskIntoConstraints = false
-        questionTextField.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            answerLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-            answerLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-
-            questionTextField.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -80),
-            questionTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            questionTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            questionTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
-        ])
-
-        answerLabel.text = L10n.Answer.empty
-        answerLabel.textColor = Asset.Colors.blackColor.color
-
-        questionTextField.borderStyle = .roundedRect
-        questionTextField.textAlignment = .center
-        questionTextField.backgroundColor = Asset.Colors.grayColor.color
-        questionTextField.placeholder = L10n.Question.Placeholder.text
-    }
-
     @objc private func rigthButtonClicked() {
         let settingsVC = SettingsViewController(storageDataProvider: StorageService())
         self.navigationController?.pushViewController(settingsVC, animated: true)
@@ -97,6 +69,7 @@ class MainViewController: UIViewController {
 
 // MARK: - NetworkServiceDelegate
 extension MainViewController: NetworkServiceDelegate {
+    
     func didGetError() {
         let customAnswer = storageDataProvider.readData(for: L10n.Answer.Custom.key)
         guard let answer = customAnswer, !answer.isEmpty else {
@@ -108,5 +81,56 @@ extension MainViewController: NetworkServiceDelegate {
 
     func didFetchMagic(_ magicData: MagicData) {
         answerLabel.text = magicData.answer
+    }
+}
+
+// MARK: - UI Configure
+private extension MainViewController {
+    
+    func setupNavigationBar() {
+        self.navigationController?.navigationBar.topItem?.title = L10n.AppName.text
+        self.navigationController?.navigationBar.tintColor = Asset.Colors.grayColor.color
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(rigthButtonClicked))
+    }
+    
+    func setupAnswerLabelCostraints() {
+        view.addSubview(answerLabel)
+        
+        answerLabel.snp.makeConstraints ({
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(self.view.snp.centerY)
+            $0.bottom.lessThanOrEqualToSuperview().inset(verticalPadding)
+            $0.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            
+        })
+    }
+    
+    func setupQuestionTextFieldCostraints() {
+        view.addSubview(questionTextField)
+        
+        questionTextField.snp.makeConstraints ({
+            $0.leading.trailing.equalToSuperview().inset(horizontalPadding)
+            $0.centerXWithinMargins.equalToSuperview()
+            $0.bottom.equalTo(self.view.snp.centerY).offset(-verticalPadding)
+            $0.height.equalTo(35)
+        })
+    }
+    
+    func setupViews() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dissmissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        view.backgroundColor = Asset.Colors.whiteColor.color
+
+        answerLabel.numberOfLines = 0
+        answerLabel.text = L10n.Answer.empty
+        answerLabel.textColor = Asset.Colors.blackColor.color
+        answerLabel.backgroundColor = .cyan
+        answerLabel.textAlignment = .natural
+
+        questionTextField.borderStyle = .roundedRect
+        questionTextField.textAlignment = .natural
+        questionTextField.backgroundColor = Asset.Colors.grayColor.color
+        questionTextField.placeholder = L10n.Question.Placeholder.text
     }
 }
